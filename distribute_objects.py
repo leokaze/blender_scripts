fd00ea9ef0ad9064355c082399e07ae013e1df18
leokaze <leokaze@users.noreply.github.com>
2024-01-17 15:59:30 -0400

    Added math.sqrt calculation
    added calculation of the square root of the number of objects to distribute them squarely in the world

--- distribute_objects.py
+++ distribute_objects.py
@@ -1,13 +1,16 @@
 import bpy
+import math
 
 i = 0
 j = 0
 
-max_i = 10
+amount_objects = len(bpy.context.selected_objects)
+max_i = math.sqrt(amount_objects)
+
 separation = 1
 
 for obj in bpy.context.selected_objects:
-    if i == max_i:
+    if i >= max_i:
         i = 0
         j += separation
     obj.location.x = i
